061f398aeb1d68898c90c15998455bc4fb6699e1
Ganesh2424 <86906361+Ganesh2424@users.noreply.github.com>
2021-09-13 21:09:03 +0530

    ganesh

---
+++ src/App.js
@@ -0,0 +1,14 @@
+  
+import React from "react";
+//import Todo from "./components/todoreact/todo";
+ import Temp from "./component/Wheather/Temp";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
